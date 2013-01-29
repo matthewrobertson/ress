64ea3574d5b8aa6ef1da673c791e7ced0b3df124
Matthew Robertson <matthewrobertson03@gmail.com>
2013-01-29 01:15:25 -0800

    dynamically add controller and helper methods for identifying request type

--- lib/ress/category_collection.rb
+++ lib/ress/category_collection.rb
@@ -15,8 +15,19 @@ module Ress
 
     def add_alternate(options)
       version = AlternateVersion.new(options.delete(:name), options.delete(:media), options)
+      define_helper_method(version) if defined? ActionController::Base
       alternate_versions << version
     end
+
+    private
+
+      def define_helper_method(version)
+        method_name = "#{version.name}_request?".to_sym
+        ActionController::Base.send(:define_method, method_name) do
+          version.matches?(request.subdomain)
+        end
+        ActionController::Base.helper_method(method_name)
+      end
 
   end
 
